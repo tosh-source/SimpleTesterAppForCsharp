ed02b667cedbc2343299c566951b268416ef3dce
lin-laptop-test <todor.chupov@gmail.com>
2018-06-11 17:38:33 +0300

    some fixes on Testing class

--- SimpleTesterApp/Testing.cs
+++ SimpleTesterApp/Testing.cs
@@ -5,54 +5,47 @@ using System.Linq;
 
 namespace SimpleTesterApp
 {
-    public static class Testing
-    {
-        public static void Start(string directory, string testFilename, string testSubName, StringBuilder testSubNumber, string testInputWord, string testOutputWord)
-        {
-            TextReader reader = null;
-            StringBuilder tempValue = new StringBuilder();
+	public static class Testing
+	{
+		public static void Start(string directory, string testFilename, string testSubName, StringBuilder testSubNumber, string testInputWord, string testOutputWord)
+		{
+			TextReader reader = null;
+			int numbLength = testSubNumber.Length;
 
-            int numbLength = testSubNumber.Length;
+			try
+			{
+				reader = new StreamReader(directory + testFilename);
+				int currentSubNumber = int.Parse(testSubNumber.ToString());
 
-            try
-            {
-                reader = new StreamReader(directory + testFilename);
-
-                while (true)
-                {
-                    int currentSubNumber = int.Parse(testSubNumber.ToString());
-
+				while (true)
+				{
 					//testing programs
-                                        
-					tempValue.Clear();
-                    tempValue.Append(testSubNumber.Replace(currentSubNumber.ToString(),
-                                         (currentSubNumber++).ToString())
-                                         .ToString().PadLeft(numbLength, '0')); //Replace old number with new and fill with zeros.
 
-					testSubNumber.Clear();
-					testSubNumber.Append(tempValue);
-                }
+					//Replace old number with (increased) new one and fill with zeros. 
+					testSubNumber.Replace(currentSubNumber.ToString().PadLeft(numbLength, '0'),
+										  (++currentSubNumber).ToString().PadLeft(numbLength, '0')); 
+				}
 
-            }
-            catch (FileNotFoundException fnEx)
-            {
+			}
+			catch (FileNotFoundException fnEx)
+			{
 				ReturnInfoMessageToConsole("\n" + fnEx.Message);
-            }
-            catch (Exception ex)
-            {
+			}
+			catch (Exception ex)
+			{
 				ReturnInfoMessageToConsole("\n" + ex.Message);
 			}
-            finally
-            {
-                reader.Close();
-            }
-        }
+			finally
+			{
+				reader.Close();
+			}
+		}
 
 		private static void ReturnInfoMessageToConsole(string message)
-        {
-            Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.WriteLine(message);
-            Console.ResetColor();
-        }
-    }
+		{
+			Console.ForegroundColor = ConsoleColor.Yellow;
+			Console.WriteLine(message);
+			Console.ResetColor();
+		}
+	}
 }
